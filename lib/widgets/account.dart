74794780913361deac6669dfe33a5ac661dcb19e
Alex <alex.lucarin@gmail.com>
2023-06-17 12:53:41 +0200

    Add logout confirm

--- lib/widgets/account.dart
+++ lib/widgets/account.dart
@@ -5,6 +5,45 @@ import 'package:intelli_grow/views/login_screen.dart';
 class Account extends StatelessWidget {
   const Account({super.key});
 
+  showAlertDialog(BuildContext context) {
+    Widget cancelButton = TextButton(
+      onPressed: () {
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+      },
+      style: const ButtonStyle(
+        foregroundColor: MaterialStatePropertyAll(Colors.lightGreen),
+      ),
+      child: const Text("Cancel"),
+    );
+    Widget continueButton = TextButton(
+      onPressed: () async {
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => const LoginScreen(),
+        ));
+        await FirebaseAuth.instance.signOut();
+      },
+      style: const ButtonStyle(
+        foregroundColor: MaterialStatePropertyAll(Colors.lightGreen),
+      ),
+      child: const Text("Continue"),
+    );
+    AlertDialog alert = AlertDialog(
+      title: const Text("Logout ?"),
+      content: const Text("Are you sure you want to logout ?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,13 +64,10 @@ class Account extends StatelessWidget {
                 backgroundColor: Colors.green,
                 elevation: 0,
               ),
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) => const LoginScreen(),
-                ));
+              onPressed: () {
+                showAlertDialog(context);
               },
-              child: const Text("Disconnect"),
+              child: const Text("Logout"),
             ),
           ],
         ),
